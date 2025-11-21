65261aff6b7bc9513c981546acd1d530fb531f6f
viann <2124100011@soy.utj.edu.mx>
2025-11-20 21:54:47 -0600

    BD actualizada

--- db/canchibol.sql
+++ db/canchibol.sql
@@ -11,16 +11,37 @@ CREATE TABLE arbitro (
     fechaRegistro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE equipos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL UNIQUE,
+    representante VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE equipos 
+ADD COLUMN estado TINYINT(1) DEFAULT 1 COMMENT '1=Activo, 0=Inactivo';
+
+INSERT INTO equipos (nombre, representante, telefono, estado) VALUES 
+('Bayern', 'Luis Adan Perez', '3331757710', 1),
+('PSG', 'Luis Daniel Camacho', '3314385621', 1);
+
+
+DROP TABLE IF EXISTS partido;
+
+-- nueva tabla partido con llaves foraneas
 CREATE TABLE partido (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fecha DATE NOT NULL,
     hora TIME NOT NULL,
     cancha VARCHAR(100) NOT NULL,
-    arbitro_id INT NOT NULL,          
-    equipo1 VARCHAR(100) NOT NULL,
-    equipo2 VARCHAR(100) NOT NULL,
-    FOREIGN KEY (arbitro_id) REFERENCES arbitro(id) 
+    arbitro_id INT NOT NULL,
+    equipo1 INT NOT NULL,
+    equipo2 INT NOT NULL,
+    FOREIGN KEY (arbitro_id) REFERENCES arbitro(id),
+    FOREIGN KEY (equipo1) REFERENCES equipos(id),
+    FOREIGN KEY (equipo2) REFERENCES equipos(id)
 );
+
 
 
 CREATE TABLE `users` (
